6e96957a112c7ae9dc855f43f3d8e50ad95651c3
Amber <ting820831@gmail.com>
2019-10-19 17:21:17 -0500

    add new files

---
+++ assests/game.js
@@ -0,0 +1,20 @@
+var topics = ["dog", "cat", "tiger", "rabbit", "snake", "hourse", "sheep"];
+
+function buttonCollect() {
+  $("#buttonCollect").empty();
+
+  for (i = 0; i < topics.length; i++) {
+    // 建立原本的按鈕
+
+    var button = $("<button>");
+    a.addClass("animals");
+    // a.attr("data-name", topics[i]);
+    a.html(topics[i]);
+  }
+  console.log(topics.length);
+}
+
+var queryURL =
+  "https://api.giphy.com/v1/gifs/search" +
+  //   randomWord +
+  "&api_key=B1RJ1jEB8ZIJDLB6wH4JacJAc2QVYtjJ";
