827dde0386da9ac57477ee719bec159847d7f300
Amber <ting820831@gmail.com>
2019-10-20 18:06:21 -0500

    updated

--- assests/game.js
+++ assests/game.js
@@ -1,17 +1,19 @@
-var topics = ["dog", "cat", "tiger", "rabbit", "snake", "hourse", "sheep"];
+var topics = ["dog", "cat", "tiger", "rabbit", "snake", "horse", "sheep"];
 function displayInfo() {
-  var animal = $(this).attr("data-name");
+  var animal = $(globalThis).attr("data-name");
 
   var queryURL =
-    "https://api.giphy.com/v1/gifs/search" +
+    "https://api.giphy.com/v1/gifs/search?q=" +
     animal +
     "&api_key=B1RJ1jEB8ZIJDLB6wH4JacJAc2QVYtjJ";
+
+  console.log(queryURL);
 
   $.ajax({
     url: queryURL,
     method: "GET"
   }).done(function(response) {
-    console.log(response);
+    console.log(queryURL);
     $("#gifDisplay").html(JSON.stringify(response));
   });
 }
@@ -26,6 +28,9 @@ function renderButton() {
     a.text(topics[i]);
     $("#buttonCollect").append(a);
   }
+  $("#buttonClick").on("click", function(displayInfo) {
+    $("#buttonCollect").append("");
+  });
 }
 
 $("#buttonClick").on("click", function(event) {
